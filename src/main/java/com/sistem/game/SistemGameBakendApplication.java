8ae7e5142d23142b57b8fc03894d3c5018fb211a
danielm-qva <jmastrapa96@gmail.com>
2022-09-08 18:22:42 -0400

    init proyect,add config db.

---
+++ src/main/java/com/sistem/game/SistemGameBakendApplication.java
@@ -0,0 +1,13 @@
+package com.sistem.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemGameBakendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemGameBakendApplication.class, args);
+    }
+
+}
